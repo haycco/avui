711c8a1bc5d38e1f54a3f03a77ace98776a12537
haycco <haycco@gmail.com>
2016-11-15 10:12:08 +0800

    writing the project introduction

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-    <button type="button" class="btn btn-block btn-default">Default</button>
-    <button type="button" class="btn btn-block btn-primary">Primary</button>
-    <button type="button" class="btn btn-block btn-success">Success</button>
+  <div id="app" class="wrapper">
+
+    <!-- Main Header -->
+    <main-header></main-header>
+
+    <!-- Left side column. contains the logo and sidebar -->
+    <main-sidebar></main-sidebar>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper"></div>
+    <!--<transition name="fade" mode="out-in">-->
+      <!--<router-view class="content-wrapper"></router-view>-->
+    <!--</transition>-->
+    <!-- /.content-wrapper -->
+
+    <!-- Main Footer -->
+    <main-footer></main-footer>
+
+    <!-- Control Sidebar -->
+    <control-sidebar></control-sidebar>
+    <!-- Add the sidebar's background. This div must be placed
+     immediately after the control sidebar -->
+    <div class="control-sidebar-bg"></div>
   </div>
+  <!-- ./wrapper -->
 </template>
 
 <script>
-  import Hello from './components/Hello'
+  import MainHeader from './components/MainHeader.vue'
+  import MainFooter from './components/MainFooter.vue'
+  import MainSidebar from './components/MainSidebar.vue'
+  import ControlSidebar from './components/ControlSidebar.vue'
 
   export default {
-    name: 'app',
-    components: {
-      Hello
-    }
+    name: 'avui-starter',
+    components: { MainHeader, MainFooter, MainSidebar, ControlSidebar }
   }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; {{ year }} <a href="#">Company</a>.</strong> All rights reserved.
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'main-footer',
+    data () {
+        return { year: (new Date()).getFullYear() }
+    }
+  }
+</script>
+
+<style>
+
+</style>
